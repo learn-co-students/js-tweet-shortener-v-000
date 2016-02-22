76bd4e3260b87cf04aaadb963d6cb69d74446b09
Mitul Mistry <mitul.mistry1@gmail.com>
2016-02-22 18:57:58 +0000

    Done.

--- spec/tweetShortener.spec.js
+++ spec/tweetShortener.spec.js
@@ -3,12 +3,14 @@
 describe('tweetShortener', function() {
 
   it('replaces long words with their expected short form', function() {
+    var tweetShortener = new TweetShortener()
     var tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!";
     var substituded_tweet = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!";
     expect(tweetShortener.wordSubstituter(tweet)).toEqual(substituded_tweet);
   });
 
   it('shortens tweets in bulk', function(){
+    var tweetShortener = new TweetShortener()
     var tweet_one = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!";
     var tweet_two = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?";
     var tweet_three = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!";
@@ -22,11 +24,12 @@ describe('tweetShortener', function() {
     var tweet_five_short = "I'm running out of example tweets 4 u guys, which is weird, because I'm a writer & this is just writing & I tweet all day. 4 real, u guys. 4 real.";
 
     var tweets = [tweet_one, tweet_two, tweet_three, tweet_four, tweet_five];
-    var shortened_tweets = [tweet_one_short, tweet_two_short, tweet_three_short, tweet_four_short, tweet_five_short];
-    expect(tweetShortener.bulkShortener(tweets)).toEqual(shortened_tweets);
+    var shortenedTweets = [tweet_one_short, tweet_two_short, tweet_three_short, tweet_four_short, tweet_five_short];
+    expect(tweetShortener.bulkShortener(tweets)).toEqual(shortenedTweets);
   });
 
   it('shortens tweets longer than 140 characters only', function(){
+    var tweetShortener = new TweetShortener()
     var over140 = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!";
     var under140 = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?";
     var over140Too = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!";
@@ -39,6 +42,7 @@ describe('tweetShortener', function() {
   });
 
   it("truncates tweets over 140 characters after shortening", function(){
+    var tweetShortener = new TweetShortener()
     var longTweet = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!";
     var shortenedTweet = tweetShortener.shortenedTruncator(longTweet);
     expect(shortenedTweet.length).toEqual(140);
@@ -49,6 +53,7 @@ describe('tweetShortener', function() {
   });
 
   it("keeps shortened tweets at their original lengths", function(){
+    var tweetShortener = new TweetShortener()
     var shortTweet = "I like short tweets and I cannot lie";
     expect(tweetShortener.shortenedTruncator(shortTweet).length).toEqual(shortTweet.length);
   });
